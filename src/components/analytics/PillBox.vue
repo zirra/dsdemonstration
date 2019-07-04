7ff35a2d7eb27bc4b37e050a9ef623a25e79b97e
zirra <msullivan@heysulli.com>
2019-07-04 13:17:58 -0500

    adding pillboxes

--- src/components/analytics/PillBox.vue
+++ src/components/analytics/PillBox.vue
@@ -16,7 +16,6 @@ export default {
 .content-pillbox {
   width: 32%;
   float: left;
-  height:240px;
   background-color: #fff;
   border-radius: 15px;
   min-height: 200px; /* minimum height */
@@ -24,8 +23,8 @@ export default {
 }
 .content-pillbox-container {
   font-size: 1.2em;
-  text-align: center;
-  padding: 25px 0;
+  text-align: left;
+  padding: 15px 25px;
   color: #404040;
 }
 </style>
