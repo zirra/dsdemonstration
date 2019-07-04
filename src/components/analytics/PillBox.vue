683c5745151487b858f37c1551cf7c8c42400815
zirra <msullivan@heysulli.com>
2019-07-04 13:16:30 -0500

    adding pillboxes

---
+++ src/components/analytics/PillBox.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="content-pillbox">
+    <div class="content-pillbox-container">
+      test
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pillbox-view'
+}
+</script>
+
+<style>
+.content-pillbox {
+  width: 32%;
+  float: left;
+  height:240px;
+  background-color: #fff;
+  border-radius: 15px;
+  min-height: 200px; /* minimum height */
+  position: relative;
+}
+.content-pillbox-container {
+  font-size: 1.2em;
+  text-align: center;
+  padding: 25px 0;
+  color: #404040;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,27 @@
 <template>
-  <div class="content-container">
-    Dashboard
+  <div class="content-container">  
+    <div class="pillbox-container">
+      <pillbox-view style="margin-right:2%;"/>
+      <pillbox-view style="margin-right:2%;" />
+      <pillbox-view />
+    </div>
   </div>
 </template>
 
 <script>
+import PillBox from '@/components/analytics/PillBox'
 export default {
-
+  name: 'dashboard-view',
+  components: {
+    'pillbox-view': PillBox
+  }
 }
 </script>
 
 <style>
-
+.pillbox-container {
+  width:100%;
+  clear: both;
+  float: none;
+}
 </style>
