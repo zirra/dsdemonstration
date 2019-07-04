24686a527a97ac218bfc7bb5f564d232e31c8774
zirra <msullivan@heysulli.com>
2019-07-04 13:31:29 -0500

    adding pillboxes

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,14 @@
     <div class="pillbox-container">
       <pillbox-view style="margin-right:2%;"/>
       <pillbox-view style="margin-right:2%;" />
-      <pillbox-view />
+      <pillbox-view/>
+    </div>
+    <div style="clear:both; float:none; padding:1%;"></div>
+    <div class="dashboard-overview">
+      <div class="dashboard-maingraph">
+      </div>
+      <div class="dashboard-sponsors">
+      </div>
     </div>
   </div>
 </template>
@@ -23,5 +30,26 @@ export default {
   width:100%;
   clear: both;
   float: none;
+  margin-bottom: 32px;
+}
+.dashboard-overview {
+  width: 100%;
+  clear: both;
+  float: none;
+}
+.dashboard-maingraph {
+  width: 66%;
+  margin-right: 2%;
+  height: 480px;
+  float: left;
+  background-color: #fff;
+  border-radius: 15px;
+}
+.dashboard-sponsors {
+  width: 32%;
+  height: 480px;
+  float: left;
+  background-color: #fff;
+  border-radius: 15px;
 }
 </style>
