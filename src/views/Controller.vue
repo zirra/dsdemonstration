bd80671da47c2b43b2e759eeaafd025212e42f17
zirra <msullivan@heysulli.com>
2019-07-05 15:52:18 -0500

    controller

--- src/views/Controller.vue
+++ src/views/Controller.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="content-container">
-    Controller
+    <stadium-controller />
   </div>
 </template>
 
 <script>
+import StadiumControl from '@/components/navigation/StadiumControl'
 export default {
-
+  name: 'controller-view',
+  components: {
+    'stadium-controller': StadiumControl
+  }
 }
 </script>
 
